abc88a620a8f5db7c5e312e956433fb38eed3408
Milan Brkic <milan.brkic1998@gmail.com>
2022-03-01 00:51:06 +0100

    sign-in endpoitn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
 import  SigninButton  from './components/SigninButton'
+import {useEffect} from "react"
 
 function App() {
+  useEffect(() => {
+    if(typeof window.ethereum !== "undefined"){
+      console.log("Web3 installed!");
+    }
+   }, []);
+
   return (
     <SigninButton/>
   );
--- src/components/SigninButton.js
+++ src/components/SigninButton.js
@@ -4,9 +4,21 @@ import axios from 'axios';
 
 class SigninButton extends React.Component{
      handleClick = async () =>{
-        const url = `${Constants.BACKEND_URL}/sign-in`;
-        const response = await axios.post(url);
-        console.log(response.data);
+        const chainId = await window.ethereum.request({
+            method:"eth_chainId"
+        })
+        
+        if(parseInt(chainId,16)===31337){
+            console.log("Connected to the right chain");
+            const accounts = await window.ethereum.request({
+                method:"eth_requestAccounts"
+            })
+            const url = `${Constants.BACKEND_URL}/sign-in`;
+            await axios.post(url, {accounts})
+        } 
+        else{
+            console.log("Connected to the wrong chain");
+        } 
     }
     render(){
         return(
