8a678a15ca2d89dc420828d78c100467a0851991
Milan Brkic <milan.brkic1998@gmail.com>
2022-05-14 16:10:33 +0200

    adding for sale and edit button

--- src/components/Nft.jsx
+++ src/components/Nft.jsx
@@ -11,7 +11,13 @@ export default class Nft extends React.Component{
             <img id='nft' src={`${this.state.googleId}`} alt={`${this.state.name}`} />  
             <div className="container">
                 <h5><b>{this.state.name}</b></h5>
-                <p>{this.state.price} ETH</p>
+                <div className='sale-and-edit-div'>
+                    <p className='price'>{this.state.price} ETH</p>
+                    <div className={`sale-label ${this.state.forSale ? 'for-sale-label' : 'not-for-sale-label'}`}>
+                        {this.state.forSale ? "For Sale" : "Not For Sale"}
+                    </div>
+                    <button className='edit-btn'>Edit</button>
+                </div>
             </div>
         </div>
         )
