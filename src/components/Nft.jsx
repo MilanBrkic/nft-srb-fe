58dbd49f8f18be0c39af9d2327b434c53ee27212
Milan Brkic <milan.brkic1998@gmail.com>
2022-05-15 11:09:17 +0200

    Revert "zoom"
    This reverts commit e05fee129a06bf23b19e619cca6b560cb4f77ae8.

--- src/components/Nft.jsx
+++ src/components/Nft.jsx
@@ -5,24 +5,9 @@ export default class Nft extends React.Component{
         this.state = {...this.props.nft};
     }
 
-    enlarge= () =>{
-        this.props.onEdit(this.state.md5Hash);
-        // this.state.enlarge = true;
-        this.forceUpdate();
-    }
-
     render(){
-        console.log(this.props.visible);
-        let addStyle='card-helper';
-        if(this.props.visible===true){
-            addStyle = 'enlarge';
-        }
-        else if(this.props.visible===false){
-            addStyle='not-visible';
-        }
         return (
-        <div className={`card grid-item ${addStyle}`}>
-            {addStyle==='enlarge' ? <div className='div-for-x'><button className='x-btn'>x</button></div> : null}
+        <div className="card grid-item">
             <img id='nft' src={`${this.state.googleId}`} alt={`${this.state.name}`} />  
             <div className="container">
                 <h5><b>{this.state.name}</b></h5>
@@ -31,7 +16,7 @@ export default class Nft extends React.Component{
                     <div className={`sale-label ${this.state.forSale ? 'for-sale-label' : 'not-for-sale-label'}`}>
                         {this.state.forSale ? "For Sale" : "Not For Sale"}
                     </div>
-                    <button onClick={this.enlarge} className='edit-btn'>Edit</button>
+                    <button className='edit-btn'>Edit</button>
                 </div>
             </div>
         </div>
