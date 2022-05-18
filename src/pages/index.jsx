ee1cfe0ec7c8258f64f6e0588703a69c6e031769
Milan Brkic <milan.brkic1998@gmail.com>
2022-05-18 06:20:50 +0200

    what we do

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,8 @@ export default class NftSrb extends React.Component {
   render() {
       return (
         <div className="main-div">
-          <img src={require('../images/logo-sive-linhije.png')} alt="nftsrb"></img>
+          <img src={require('../images/logo-sive-linhije-copy.png')} alt="nftsrb"></img>
+          <h3 className='about-us-title bold-font'>What we do</h3>
         </div>
       );
   }
