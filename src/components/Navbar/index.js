44056539ef2b502bc6158de090fc63e66fc73157
Milan Brkic <milan.brkic1998@gmail.com>
2022-05-18 07:20:00 +0200

    remove logo when on home screen

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -17,21 +17,24 @@ export default class Navbar extends React.Component {
     this.setState({ accessId: getAccessToken() });
     this.forceUpdate();
   };
+
+  onLinkClik = ()=>{
+    this.forceUpdate();
+  }
   render() {
+    const show = this.state.accessId && window.location.href[window.location.href.length-1] !=='/'
     return (
       <>
         <Nav>
-          {this.state.accessId ? (
-            <NavLink to="/">
-              <img src={require('../../images/nft-srb-bey-lin.png')} width="30%" length="30%" alt="logo" />
-            </NavLink>
-          ) : null}
+          <NavLink to="/">
+            <img src={require('../../images/nft-srb-bey-lin.png')} style={{visibility: show ? 'visible' : 'hidden'}}  width="30%" length="30%" alt="logo" />
+          </NavLink>
 
           <NavMenu>
-            {this.state.accessId ? <Link className='navbar-link bold-font' to="/">nft srb</Link> : null}
-            {this.state.accessId ? <Link className='navbar-link bold-font' to="/buy-nfts">buy nfts</Link> : null}
-            {this.state.accessId ? <Link className='navbar-link bold-font' to="/my-collection">my collection</Link> : null}
-            {this.state.accessId ? <Link className='navbar-link bold-font' to="/mint-nfts">mint nfts</Link> : null}
+            {this.state.accessId ? <Link className='navbar-link bold-font' onClick={this.onLinkClik} to="/">nft srb</Link> : null}
+            {this.state.accessId ? <Link className='navbar-link bold-font' onClick={this.onLinkClik} to="/buy-nfts">buy nfts</Link> : null}
+            {this.state.accessId ? <Link className='navbar-link bold-font' onClick={this.onLinkClik} to="/my-collection">my collection</Link> : null}
+            {this.state.accessId ? <Link className='navbar-link bold-font' onClick={this.onLinkClik} to="/mint-nfts">mint nfts</Link> : null}
             <Auth onAuth={this.onAuth.bind(this)} />
           </NavMenu>
         </Nav>
