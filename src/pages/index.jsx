24e9f40bba741e173ac7f9aa02ac8008029a3881
Milan Brkic <milan.brkic1998@gmail.com>
2022-05-30 14:17:03 +0200

    added text for the home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,13 @@ export default class NftSrb extends React.Component {
       return (
         <div className="main-div">
           <img src={require('../images/logo-sive-linhije-copy.png')} alt="nftsrb"></img>
+          <h3 className='about-us-title bold-font'>Who are we</h3>
+          <p className='about-us-text'>First Serbian NFT marketplace.</p>
           <h3 className='about-us-title bold-font'>What we do</h3>
+          <p className='about-us-text'>
+            We bring you the most flexible and affordable marketplace in the region.<br/>
+            Your job is to make art, we'll do the rest!
+          </p>
         </div>
       );
   }
